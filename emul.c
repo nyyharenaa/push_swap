63bfe3f60d56023588151d4e1d3f81d992f4c33e
todina-r <ratsinjomananatolotra@gmail.com>
2026-03-12 11:35:37 +0300

    test simple algorithm

---
+++ emul.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   emul.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ny-handr <ny-handr@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 13:30:04 by todina-r          #+#    #+#             */
+/*   Updated: 2026/03/12 10:49:43 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	init_stack(t_stack *st);
+static void	execute_cmd_function(char *cmd, t_stack *st_a, t_stack *st_b);
+static void	handle_user_cmd(t_stack *st_a, t_stack *st_b);
+static void	print_stack_a_b(t_stack st_a, t_stack st_b);
+
+int	main(int ac, char **av)
+{
+	t_stack		st_a;
+	t_stack		st_b;
+	t_st_node	*node;
+	int			index;
+	int			value;
+
+	index = 1;
+	init_stack(&st_a);
+	init_stack(&st_b);
+	while (index < ac)
+	{
+		value = ft_atoi(av[index]);
+		node = st_new(value);
+		st_add_back(&st_a, node);
+		index++;
+	}
+	handle_user_cmd(&st_a, &st_b);
+	st_clear(&st_a);
+	st_clear(&st_b);
+	return (0);
+}
+
+static void	init_stack(t_stack *st)
+{
+	st->first = NULL;
+	st->last = NULL;
+	st->size = 0;
+}
+
+static void	print_stack_a_b(t_stack st_a, t_stack st_b)
+{
+	t_st_node	*node;
+
+	node = st_a.first;
+	ft_printf("a: ");
+	while (node)
+	{
+		ft_printf("%i", node->value);
+		node = node->next;
+		if (node)
+			ft_printf(" ");
+	}
+	ft_printf("\n");
+	node = st_b.first;
+	ft_printf("b: ");
+	while (node)
+	{
+		ft_printf("%i", node->value);
+		node = node->next;
+		if (node)
+			ft_printf(" ");
+	}
+	ft_printf("\n");
+}
+
+static void	execute_cmd_function(char *cmd, t_stack *st_a, t_stack *st_b)
+{
+	if (ft_strncmp(cmd, "sa", 2) == 0)
+		sa(st_a, st_b);
+	else if (ft_strncmp(cmd, "sb", 2) == 0)
+		sb(st_a, st_b);
+	else if (ft_strncmp(cmd, "ss", 2) == 0)
+		ss(st_a, st_b);
+	else if (ft_strncmp(cmd, "pa", 2) == 0)
+		pa(st_a, st_b);
+	else if (ft_strncmp(cmd, "pb", 2) == 0)
+		pb(st_a, st_b);
+	else if (ft_strncmp(cmd, "rra", 3) == 0)
+		rra(st_a, st_b);
+	else if (ft_strncmp(cmd, "rrb", 3) == 0)
+		rrb(st_a, st_b);
+	else if (ft_strncmp(cmd, "rrr", 3) == 0)
+		rrr(st_a, st_b);
+	else if (ft_strncmp(cmd, "ra", 2) == 0)
+		ra(st_a, st_b);
+	else if (ft_strncmp(cmd, "rb", 2) == 0)
+		rb(st_a, st_b);
+	else if (ft_strncmp(cmd, "rr", 2) == 0)
+		rr(st_a, st_b);
+}
+
+static void	handle_user_cmd(t_stack *st_a, t_stack *st_b)
+{
+	char	*line;
+	char	**cmd_list;
+	int		index;
+	int		stop;
+
+	line = "";
+	stop = 0;
+	while (!stop && line)
+	{
+		index = 0;
+		cmd_list = NULL;
+		line = get_next_line(0);
+		if (line)
+			cmd_list = ft_split(line, ' ');
+		while (cmd_list && cmd_list[index])
+		{
+			execute_cmd_function(cmd_list[index], st_a, st_b);
+			free(cmd_list[index++]);
+		}
+		print_stack_a_b(*st_a, *st_b);
+		if (cmd_list)
+			free(cmd_list);
+		if (line)
+			free(line);
+	}
+}
--- main.c
+++ main.c
@@ -6,15 +6,13 @@
 /*   By: ny-handr <ny-handr@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 13:30:04 by todina-r          #+#    #+#             */
-/*   Updated: 2026/03/11 12:26:04 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/03/12 10:56:11 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static void	init_stack(t_stack *st);
-static void	*get_cmd_function(char *cmd);
-static void	handle_user_cmd(t_stack *st_a, t_stack *st_b);
 static void	print_stack_a_b(t_stack st_a, t_stack st_b);
 
 int	main(int ac, char **av)
@@ -23,19 +21,21 @@ int	main(int ac, char **av)
 	t_stack		st_b;
 	t_st_node	*node;
 	int			index;
-	int			value;
+	t_list		*oplist;
 
 	index = 1;
 	init_stack(&st_a);
 	init_stack(&st_b);
 	while (index < ac)
 	{
-		value = ft_atoi(av[index]);
-		node = st_new(value);
+		node = st_new(ft_atoi(av[index]));
 		st_add_back(&st_a, node);
 		index++;
 	}
-	handle_user_cmd(&st_a, &st_b);
+	oplist = al_simple(&st_a, &st_b);
+	oplst_print(oplist);
+	oplst_clear(&oplist);
+	print_stack_a_b(st_a, st_b);
 	st_clear(&st_a);
 	st_clear(&st_b);
 	return (0);
@@ -72,64 +72,4 @@ static void	print_stack_a_b(t_stack st_a, t_stack st_b)
 			ft_printf(" ");
 	}
 	ft_printf("\n");
-}
-
-static void	*get_cmd_function(char *cmd)
-{
-	if (ft_strncmp(cmd, "sa", 2) == 0)
-		return (sa);
-	else if (ft_strncmp(cmd, "sb", 2) == 0)
-		return (sb);
-	else if (ft_strncmp(cmd, "ss", 2) == 0)
-		return (ss);
-	else if (ft_strncmp(cmd, "pa", 2) == 0)
-		return (pa);
-	else if (ft_strncmp(cmd, "pb", 2) == 0)
-		return (pb);
-	else if (ft_strncmp(cmd, "rra", 3) == 0)
-		return (rra);
-	else if (ft_strncmp(cmd, "rrb", 3) == 0)
-		return (rrb);
-	else if (ft_strncmp(cmd, "rrr", 3) == 0)
-		return (rrr);
-	else if (ft_strncmp(cmd, "ra", 2) == 0)
-		return (ra);
-	else if (ft_strncmp(cmd, "rb", 2) == 0)
-		return (rb);
-	else if (ft_strncmp(cmd, "rr", 2) == 0)
-		return (rr);
-	return (NULL);
-}
-
-static void	handle_user_cmd(t_stack *st_a, t_stack *st_b)
-{
-	void	(*func)(t_stack *, t_stack *);
-	char	*line;
-	char	**cmd_list;
-	int		index;
-	int		stop;
-
-	stop = 0;
-	while (!stop)
-	{
-		index = 0;
-		cmd_list = NULL;
-		line = get_next_line(0);
-		if (line)
-			cmd_list = ft_split(line, ' ');
-		while (cmd_list && cmd_list[index])
-		{
-			func = get_cmd_function(cmd_list[index]);
-			if (func)
-				func(st_a, st_b);
-			free(cmd_list[index++]);
-		}
-		print_stack_a_b(*st_a, *st_b);
-		if (cmd_list)
-			free(cmd_list);
-		if (line)
-			free(line);
-		else
-			stop = 1;
-	}
 }

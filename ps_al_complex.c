7c714c7d9292064912d7387614222ac956871c78
todina-r <ratsinjomananatolotra@gmail.com>
2026-03-27 15:21:55 +0300

    correction de mon erreur dans ps_al_complex

--- ps_al_complex.c
+++ ps_al_complex.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 19:16:31 by todina-r          #+#    #+#             */
-/*   Updated: 2026/03/27 13:21:59 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/03/27 15:21:05 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,26 @@ t_list	*al_complex(t_stack *st_a, t_stack *st_b)
 {
 	size_t	i;
 	size_t	j;
-	t_list	*oplst;
+	size_t	size;
 	size_t	max_bits;
+	t_list	*oplst;
 
 	i = 0;
 	oplst = NULL;
+	size = st_a->size;
 	max_bits = get_max_bits(st_a);
-	while (i < max_bits)
+	while (i++ < max_bits)
 	{
 		j = 0;
-		while (j < st_a->size)
+		while (j++ < size)
 		{
-			if (!((st_a->first->value >> i) & 1))
+			if (!((st_a->first->value >> (i - 1)) & 1))
 				pb(st_a, st_b, &oplst);
 			else
 				ra(st_a, st_b, &oplst);
-			j++;
 		}
 		while (st_b->size > 0)
 			pa(st_a, st_b, &oplst);
-		i++;
 	}
 	return (oplst);
 }
